f5b596e85b2923be6e1c1b5f071e0fe2759e4acf
Sanchit Behl <48411698+JWALAA@users.noreply.github.com>
2019-06-05 14:21:58 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import bs4
+import json
+import pandas as pd
+import requests
+
+
+State = []
+District =[]
+A_C = []
+
+driver = webdriver.Chrome('C:\\Users\\behl\\AppData\\Local\\Programs\\Python\\Python36-32\Scripts\\chromedriver.exe')
+
+driver.get("http://psleci.nic.in")
+
+content = driver.page_source
+
+soup = bs4.BeautifulSoup(content, 'html5lib')
+
+
+for a in soup.findAll('div',attrs={'class':'style1'}):
+    State1=a.find('select', attrs={'id':'ddlState'})
+    District1=a.find('select', attrs={'id':'ddlDistrict'})
+    A_C1=a.find('select', attrs={'id':'ddlAC'})
+State.append(State1)
+District.append(District1)
+A_C.append(A_C1)
+
+df = pd.DataFrame({'State': State,'District':District,'A_C':A_C})
+df.to_csv('C:\\Users\\behl\\Desktop\\vote.csv', index=False, encoding='utf-8')
