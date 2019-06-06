a14efda1124fda5bb4ecd5baaa7ec1edfa766c3b
Sanchit Behl <48411698+JWALAA@users.noreply.github.com>
2019-06-06 22:06:19 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,26 +1,62 @@
 from selenium import webdriver
-import bs4
+from selenium.webdriver.support.ui import Select
+import json
 
-State = []
-District =[]
-A_C = []
+url="http://psleci.nic.in/"
 
-driver = webdriver.Chrome('C:\\Users\\behl\\AppData\\Local\\Programs\\Python\\Python36-32\Scripts\\chromedriver.exe')
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe"
+driver = webdriver.Chrome('chromedriver.exe', options=options)
+driver.get(url)
 
-driver.get("http://psleci.nic.in")
+State1=Select(driver.find_element_by_id("ddlState"))
+print(len(State1.options))
+state = State1.options
 
-content = driver.page_source
+for i in State1.options:
+    print(i.text)
 
-soup = bs4.BeautifulSoup(content, 'html5lib')
+l3={}
 
+for i in range(1,len(State1.options)):
+    district=[]
+    l2={}
+    State1=Select(driver.find_element_by_id("ddlState"))
+    State1.options[i].click()
+    District1=Select(driver.find_element_by_id("ddlDistrict"))
+    for a in District1.options:
+        district.append(a.text)
+    district=district[1:]
+    for j in range(1,len(District1.options)):
+        l1={}
+        District1=Select(driver.find_element_by_id("ddlDistrict"))
+        District1.options[j].click()
+        ac1=Select(driver.find_element_by_id("ddlAC"))
+        ac=[]
+        for a in z.options:
+            ac.append(a.text)
+        ac=ac[1:]
+        for k in range(1,len(ac1.options)):
+            ac1=Select(driver.find_element_by_id("ddlAC"))
+            ac1.options[k].click()
+            l=Select(driver.find_element_by_id("ddlPS"))
+            pollst=[]
+            for a in l.options:
+                pollst.append(a.text)
+            pollst=pollst[1:]
+            try:
+                l1[ac[k]]=pollst
+            except:
+                print("AC Error")
+        try:
+            l2[district[j]]=l1
+        except:
+            print("District Error")
+    try:
+        l3[state[i].text]=l2
+    except:
+        print("State Error")    
 
-for a in soup.findAll('div',attrs={'class':'style1'}):
-    State1=a.find('select', attrs={'id':'ddlState'})
-    District1=a.find('select', attrs={'id':'ddlDistrict'})
-    A_C1=a.find('select', attrs={'id':'ddlAC'})
-State.append(State1)
-District.append(District1)
-A_C.append(A_C1)
-
-df = pd.DataFrame({'State': State,'District':District,'A_C':A_C})
-df.to_csv('C:\\Users\\behl\\Desktop\\vote.csv', index=False, encoding='utf-8')
+with open("poll.json", "a") as write_file:
+    json.dump(l3, write_file, indent=4)
